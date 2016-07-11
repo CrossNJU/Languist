676477542e6632908987a03a47197197d4e962bc
PolarisChen <polaris_chen@163.com>
2016-07-11 10:56:32 +0800

    Add HomePage component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,144 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './HomePage.scss';
+
+class Cover extends Component {
+  render() {
+    return (
+      <div className="cover">
+        <img className="avatar" src={require('./avatar-default.png')} width="112" height="112" alt="Avatar" />
+        <h1>Polaris Chen</h1>
+        <span>8 Languages</span>
+      </div>
+    );
+  }
+}
+
+class Count extends Component {
+  render() {
+    let data = {
+      followingCount: 6,
+      followersCount: 12,
+      starredCount: 32
+    };
+    return (
+      <div className="Card">
+        <div className="CountItem">
+          <div className="CountNumber">{data.followersCount}</div>
+          <div className="CountLabel">Followers</div>
+        </div>
+        <div className="CountItem">
+          <div className="CountNumber">{data.starredCount}</div>
+          <div className="CountLabel">Starred</div>
+        </div>
+        <div className="CountItem">
+          <div className="CountNumber">{data.followingCount}</div>
+          <div className="CountLabel">Following</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+let repoData = [
+  {
+    avatarUrl: '',
+    owner: 'facebook',
+    name: 'react',
+    description: 'A declarative, efficient, and flexible JavaScript library for building user interfaces. https://facebook.github.io/react/',
+    tags: [
+      'JavaScript',
+      'Framework'
+    ]
+  },
+  {
+    avatarUrl: '',
+    owner: 'callemall',
+    name: 'material-ui',
+    description: 'React Components that Implement Google\'s Material Design. http://www.material-ui.com',
+    tags: [
+      'JavaScript',
+      'Framework',
+      'React',
+      'UI'
+    ]
+  }
+];
+
+class RepoItem extends Component {
+  render() {
+    return (
+      <div>
+        <img className="avatar" src={require('./avatar-default.png')} width="36" height="36" alt="Avatar" />
+        <h3>{this.props.repo.owner}/{this.props.repo.name}</h3>
+        <p>{this.props.repo.description}</p>
+        <div className="tags">
+          {
+            this.props.repo.tags.map((tag, i) => {
+              return <div key={i}>{tag}</div>;
+            })
+          }
+        </div>
+        <div className="actions">
+          <button>STAR</button>
+          <button>VIEW</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class RepoList extends Component {
+  renderRepos() {
+    let repos = repoData.map(repo => {
+      console.log(repo);
+      return (<RepoItem key={repo.owner + repo.name} repo={repo} />);
+    });
+    return repos;
+  };
+
+  render() {
+    return (
+      <div>
+        {this.renderRepos()}
+      </div>
+    );
+  }
+}
+
+const title = 'Home';
+
+class HomePage extends Component {
+
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired,
+  };
+
+  componentWillMount() {
+    this.context.onSetTitle(title);
+  }
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <Cover />
+          <Count />
+          <RepoList />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default withStyles(HomePage, s);
