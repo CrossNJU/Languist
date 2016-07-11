8711eebe22285aa2156548fd41678f885de9fa93
PolarisChen <polaris_chen@163.com>
2016-07-11 15:03:38 +0800

    Customize UI theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,42 @@ import s from './App.scss';
 import Header from '../Header';
 import Footer from '../Footer';
 
+import {
+  cyan500, cyan700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: cyan500,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: '#F2DF83',
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+  appBar: {
+    color: '#F2DF83',
+  }
+});
+
 class App extends Component {
 
   static propTypes = {
@@ -54,11 +90,13 @@ class App extends Component {
 
   render() {
     return !this.props.error ? (
+      <MuiThemeProvider muiTheme={muiTheme}>
       <div>
         <Header />
         {this.props.children}
         <Footer />
       </div>
+      </MuiThemeProvider>
     ) : this.props.children;
   }
 
