d5456b9fe1d37f85f7f98a8359ec2cc465906fc6
PolarisChen <polaris_chen@163.com>
2016-07-11 10:56:58 +0800

    Update App component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,13 +21,9 @@ class Header extends Component {
         <div className={s.container}>
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
-            <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <img src={require('./logo-brand.png')} width="36" height="36" alt="Languist" />
+            <span className={s.brandTxt}>Languist</span>
           </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
         </div>
       </div>
     );
