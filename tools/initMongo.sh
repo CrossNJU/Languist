fc75faea26223fec31abd3041fd20c8c6c28a786
PolarisChen <polaris_chen@163.com>
2016-08-04 14:06:56 +0800

    Add script for mongo initiation

---
+++ tools/initMongo.sh
@@ -0,0 +1,4 @@
+mongoimport -d languist -c users --type csv --file data/users.csv --headerline
+mongoimport -d languist -c languages --type csv --file data/languages.csv --headerline
+mongoimport -d languist -c github_users --type csv --file data/github_users.csv --headerline
+mongoimport -d languist -c github_repos --type csv --file data/github_repos.csv --headerline
