e99f233ece5876a603fba8dd45774b60883e4a45
cdn14 <cdn14@software.nju.edu.cn>
2016-08-16 21:40:53 +0800

    Update recommend

--- src/servers/logic/RecommendLogic_languages.js
+++ src/servers/logic/RecommendLogic_languages.js
@@ -2,6 +2,12 @@
  * Created by ChenDanni on 2016/8/15.
  */
 
+import {getLanguageByUser} from '../dao/languageDAO'
+import {getLanguageByUser} from '../dao/languageDAO'
+import {getLanguageByTag} from '../dao/languageDAO'
+import {getLanguageSize} from '../dao/languageDAO'
+import {connect} from '../config'
+
 let rec_langs = [];
 let user_langs = [];
 let user_tags = [];
@@ -15,43 +21,46 @@ function getSortFun(order, sortBy) {
   return sortFun;
 }
 
-//test
-function getLanguageByUser(login){
-  return [{
-    name: "lan1",
-    level: 1,
-    tag:["tag1","tag2","tag3"],
-    size:100
-  },{
-    name: "lan2",
-    level: 2,
-    tag:["tag2","tag3","tag4"],
-    size:100
-  },{
-    name: "lan3",
-    level: 1,
-    tag:["tag1","tag4","tag3"],
-    size:100
-  }];
-}
-
-function getLanguageByTag(tag){
-  if (tag == 'tag1') {
-    return [{'name':'lan1'},{'name':'lan2'},{'name':'lan3'}];
-  }
-  if (tag == 'tag2') {
-    return [{'name':'lan2'},{'name':'lan3'},{'name':'lan4'}];
-  }
-  if (tag == 'tag3') {
-    return [{'name':'lan3'},{'name':'lan4'},{'name':'lan6'}];
-  }
-  if (tag == 'tag4') {
-    return [{'name':'lan4'},{'name':'lan5'},{'name':'lan6'}];
-  }
-  return {}
-}
-//test
-
+////test
+//function getLanguageByUser(login){
+//  return [{
+//    name: "lan1",
+//    level: 1,
+//    tag:["tag1","tag2","tag3"],
+//    size:100
+//  },{
+//    name: "lan2",
+//    level: 2,
+//    tag:["tag2","tag3","tag4"],
+//    size:100
+//  },{
+//    name: "lan3",
+//    level: 1,
+//    tag:["tag1","tag4","tag3"],
+//    size:100
+//  }];
+//}
+//
+//function getLanguageByTag(tag){
+//  if (tag == 'tag1') {
+//    return [{'name':'lan1'},{'name':'lan2'},{'name':'lan3'}];
+//  }
+//  if (tag == 'tag2') {
+//    return [{'name':'lan2'},{'name':'lan3'},{'name':'lan4'}];
+//  }
+//  if (tag == 'tag3') {
+//    return [{'name':'lan3'},{'name':'lan4'},{'name':'lan6'}];
+//  }
+//  if (tag == 'tag4') {
+//    return [{'name':'lan4'},{'name':'lan5'},{'name':'lan6'}];
+//  }
+//  return {}
+//}
+//
+//function getLanguageSize(language){
+//  return 1000;
+//}
+////test
 
 //得到user的tag情况
 //user_tags{
@@ -92,7 +101,7 @@ function get_user_tag(login){
 function get_rec_languages(login){
   let lang_info = [];
 
-  user_langs = getLanguageByUser(login);
+  user_langs = await getLanguageByUser(login);
 
   user_tags = get_user_tag(login);
 
@@ -126,37 +135,32 @@ function get_rec_languages(login){
     }
   }
 
-  console.log(lang_count);
-
+  //将所得语言按流行度和用户语言tag统计结果排序
   for (let key in lang_count){
     let temp_lang = {
       name: key,
-      count: lang_count[key],
-      size: getLanguageSize(key)
-    }
+      score: lang_count[key] + getLanguageSize(key)/5000
+    };
     lang_info.push(temp_lang);
   }
+  lang_info.sort(getSortFun('desc', 'score'));
 
-
-  //将所得语言按流行度排序（暂时是size）
-  lang_count.sort(function(a,b){
-    return a<b?1:-1
-  });
   //取前rec_num返回作为推荐语言
   let re = 0;
-  for (let lang in lang_count){
-    let temp_lan = {
-      type: 'lang',
-      name: lang,
-      description: findLanguageByName(lang).description
-    }
-    rec_langs[re] = temp_lan;
+  let wlen = lang_info.length;
+  for (let i = 0;i < wlen;i++){
+    if (wlen < re){break;}
+
+    rec_langs.push(lang_info[i].name);
     re++;
     if (re == rec_num){
       re = 0;
       break;
     }
+
   }
+
+  //console.log(rec_langs);
 
   return rec_langs;
 }
