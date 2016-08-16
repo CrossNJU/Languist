ad94138bdb38f0baf3e25ad11b921e11986dd4ab
RayChen <raychen@Ray-Chens-MacBook-Pro.local>
2016-08-16 22:39:17 +0800

    add repodao

--- src/servers/dao/RepoDAO.js
+++ src/servers/dao/RepoDAO.js
@@ -3,14 +3,26 @@
  */
 
 import {userSchema} from '../../models/userSchema'
+import {github_repoSchema} from '../../models/github_repoSchema'
 import {connect_callback} from '../config'
 
 async function getStarRepoByUser(login){
   let t = await new Promise(function (resolve, reject) {
-    let ans = [];
-    userSchema.findOne({login: login}, (err, user) => {
+    userSchema.findOne({login: login}, async (err, user) => {
       if (err) reject(err);
-      ans = user.star_repos;
+      let ans = [];
+      for (let repo of user.star_repos){
+        let repo_det = await new Promise(function(resolve2, reject2) {
+          github_repoSchema.findOne({full_name: repo}, (err, repo_one) => {
+            if (err) reject2(err);
+            resolve2(repo_one.stars_count);
+          });
+        });
+        ans.push({
+          fullname: repo,
+          stars: repo_det
+        })
+      }
       resolve(ans);
     });
   });
@@ -43,6 +55,8 @@ async function getUserAndLevelByLanguage(language){
   });
   return promise;
 }
+
+export {getStarRepoByUser, getUserAndLevelByLanguage}
 
 async function test(){
   connect_callback(async (v) => {
