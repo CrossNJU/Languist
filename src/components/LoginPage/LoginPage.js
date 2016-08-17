a91146f18defdaec45e2b5c0cfe79fa67c423067
chenmuen <593827475@qq.com>
2016-08-17 17:41:58 +0800

    Update LoginPage

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -10,29 +10,13 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
-import Link from '../Link';
-
 import s from './LoginPage.scss';
 import $ from 'jquery';
 
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import Divider from 'material-ui/Divider';
+
+import LoginCard from '../LoginCard';
 
 const title = 'Log In';
-
-const dividerStyle = {
-  marginTop: '10px',
-  marginBottom: '10px',
-  height: '2px'
-};
-const buttonLableStyle = {
-  fontWeight: 'normal'
-};
-const buttonStyle = {
-  marginTop: '10px'
-};
 
 class LoginPage extends Component {
   constructor(props) {
@@ -47,32 +31,26 @@ class LoginPage extends Component {
     this.context.onSetTitle(title);
   }
 
-  handleLogin() {
+  handleLogin(username, password) {
     let url = '/api/login';
-    let username = this.refs.username.input.value;
-    let password = this.refs.password.input.value;
 
     $.ajax(url, {data:{username: username, password: password}})
       .done((message) => {
         console.log(message);
+        if(message == "success") {
+          window.location.href = "/home";
+        } else {
+          console.log('fail');
+        }
       })
+
   }
 
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <Paper className={s.content}>
-            <p className={s.title}>Hi, Languist :-)</p>
-            <TextField hintText="Username" fullWidth={true} ref="username"/>
-            <TextField hintText="Password" type="password"  fullWidth={true} ref="password"/>
-            <RaisedButton label="LOGIN" primary={true} style={buttonStyle} labelStyle={buttonLableStyle} onClick={this.handleLogin.bind(this)}/>
-            <Link className={s.link} to="/">
-              Forget password?
-            </Link>
-            <Divider style={dividerStyle}/>
-            <RaisedButton label="SIGN UP WITH GITHUB" href="https://github.com/login/oauth/authorize?client_id=d310933db63d64f563a0" style={buttonStyle} secondary={true}  labelStyle={buttonLableStyle}/>
-          </Paper>
+          <LoginCard handleSubmit={this.handleLogin.bind(this)} type="login"/>
         </div>
       </div>
     );
