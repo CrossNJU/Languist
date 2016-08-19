920a9c8483005afe8512ab21aaaf2b790040bd73
PolarisChen <polaris_chen@163.com>
2016-08-19 22:00:03 +0800

    Update initMongo shell

--- tools/initMongo.sh
+++ tools/initMongo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-mongoimport -d languist -c users --type csv --file data/users.csv --headerline
-mongoimport -d languist -c languages --type csv --file data/languages.csv --headerline
-mongoimport -d languist -c github_users --type csv --file data/github_users.csv --headerline
-mongoimport -d languist -c github_repos --type csv --file data/github_repos.csv --headerline
+# mongoimport -d languist -c users --type csv --file data/users.csv --headerline
+# mongoimport -d languist -c languages --type csv --file data/languages.csv --headerline
+# mongoimport -d languist -c github_users --type csv --file data/github_users.csv --headerline
+# mongoimport -d languist -c github_repos --type csv --file data/github_repos.csv --headerline
+
+mongoimport -d languist -c users --file data/users.json
+mongoimport -d languist -c languages --file data/languages.json
+mongoimport -d languist -c github_repos --file data/github_repos.json
