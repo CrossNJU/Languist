c73367f1f96007f17916112cdd35ea69cad4c1b6
PolarisChen <polaris_chen@163.com>
2016-09-09 12:45:18 +0800

    Add RepoSetFilter

---
+++ src/components/RepoSetFilter/RepoSetFilter.js
@@ -0,0 +1,35 @@
+/**
+ * Created by PolarisChen on 16/9/9.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './RepoSetFilter.scss';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+import Filter from '../Filter';
+
+class RepoSetFilter extends Component {
+  constructor(props) {
+    super(props);
+  }
+  renderAction() {
+    return (
+      <div className={s.action}>
+        <RaisedButton
+          fullWidth={true}
+          primary={true}
+          label="Add New Repo Set"
+        />
+      </div>
+    )
+  }
+  render() {
+    return (
+      <Filter current={this.props.current} data={this.props.data}>{this.renderAction()}</Filter>
+    );
+  }
+}
+
+export default withStyles(RepoSetFilter, s);
