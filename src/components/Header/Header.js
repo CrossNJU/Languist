4dfe733ddcd15ac0b9cf91bacd478b4d16670453
chenmuen <593827475@qq.com>
2016-09-13 00:09:30 +0800

    Update Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,11 +9,24 @@ import Link from '../Link';
 import Navigation from '../Navigation';
 
 import Paper from 'material-ui/Paper';
+import FeedbackDialog from '../FeedbackDialog';
 
 class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isDialogOpen: false
+    }
   }
+
+  handleFeedbackDialogOpen() {
+    this.setState({isDialogOpen: true});
+  }
+
+  handleFeedbackDialogClose() {
+    this.setState({isDialogOpen: false});
+  }
+
   render() {
     return (
       <div className={s.root}>
@@ -21,8 +34,9 @@ class Header extends Component {
           <Link className={s.brand} to="/home">
             <img src={require('./logo-brand-text@2x.png')} height="32" alt="Languist" />
           </Link>
-          <Navigation className={s.nav} login={this.props.login} handleLogout={this.props.handleLogout} />
+          <Navigation className={s.nav} login={this.props.login} handleLogout={this.props.handleLogout} handleFeedback={this.handleFeedbackDialogOpen.bind(this)}/>
         </div>
+        <FeedbackDialog isOpen={this.state.isDialogOpen} user={this.props.login} handleClose={this.handleFeedbackDialogClose.bind(this)}/>
       </div>
     );
   }
