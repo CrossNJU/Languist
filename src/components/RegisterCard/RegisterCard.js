b85769f931fbc4d3f4f7e60a586231914954621f
chenmuen <593827475@qq.com>
2016-09-13 00:08:43 +0800

    Update RegisterPage

--- src/components/RegisterCard/RegisterCard.js
+++ src/components/RegisterCard/RegisterCard.js
@@ -9,17 +9,16 @@
 
 import React, {Component, PropTypes} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import $ from 'jquery';
 
 import Link from '../Link';
 
-import s from './LoginCard.scss';
+import s from './RegisterCard.scss';
 
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
-
-const title = 'Log In';
 
 const dividerStyle = {
   marginTop: '10px',
@@ -33,9 +32,19 @@ const buttonStyle = {
   marginTop: '15px'
 };
 
-class LoginCard extends Component {
+class RegisterCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      login: '',
+      comfirm
+    }
+  }
+
+  async componentDidMount() {
+    let url = '/api/temp_user';
+    let user =  await $.ajax(url);
+    this.setState({login: user});
   }
 
   handleSubmit() {
@@ -55,26 +64,18 @@ class LoginCard extends Component {
     return (
       <Paper className={s.content}>
         <p className={s.title}>Hi, Languist :-)</p>
-        <TextField hintText="Username" fullWidth={true} ref="username" errorText={this.props.userNameError} onKeyPress={this.handleEnter.bind(this)}/>
+        <TextField hintText="Username" fullWidth={true} ref="username" disabled={true}  value={this.state.login}/>
         <TextField hintText="Password" type="password" fullWidth={true} ref="password" errorText={this.props.passwordError} onKeyPress={this.handleEnter.bind(this)}/>
-        <RaisedButton
-          label={"LOGIN"}
-          primary={true}
-          style={buttonStyle}
-          labelStyle={buttonLableStyle}
-          onClick={this.handleSubmit.bind(this)}/>
-        <Link className={s.link} to="/">
-          Forget password?
-        </Link>
-        <Divider style={dividerStyle}/>
-        <RaisedButton label="SIGN UP WITH GITHUB"
-                      href="https://github.com/login/oauth/authorize?client_id=c1bb199ad072f9f1639d"
-                      style={buttonStyle} secondary={true}
-                      labelStyle={buttonLableStyle}/>
+        <TextField hintText="Confirm Password" type="password" fullWidth={true} ref="password_again"/>
+        <RaisedButton label={"SIGN UP"}
+                      primary={true}
+                      style={buttonStyle}
+                      labelStyle={buttonLableStyle}
+                      onClick={this.handleSubmit.bind(this)}/>
       </Paper>
     );
   }
 
 }
 
-export default withStyles(LoginCard, s);
+export default withStyles(RegisterCard, s);
