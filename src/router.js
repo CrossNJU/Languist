ed60d51dd618b4bb90a6f715b55188fbe3d011df
RickChem <1395314348@qq.com>
2016-09-20 01:25:08 +0800

    fix bug

--- src/router.js
+++ src/router.js
@@ -72,8 +72,10 @@ function addRepoAPI(server) {
   //search repo
   server.get('/api/search/repo', (req, res) => {
     searchRepo(req.query.keyword, req.query.language, req.query.page, (resa) => {
-      record_log(getUser(), getUser() + ' search repo: ' + req.query.q + ' and return: ' + resa.length + ' RECORDS', 'query');
-      res.send(resa);
+      addInfoToList(getUser(), resa, true, () => {
+        record_log(getUser(), getUser() + ' search repo: ' + req.query.q + ' and return: ' + resa.length + ' RECORDS', 'query');
+        res.send(resa);
+      });
     })
   });
   //get set list
